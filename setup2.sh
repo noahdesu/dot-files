ed14b93d9a64edd4da1106cde4e0988b0811cb75
Noah Watkins <noahwatkins@gmail.com>
2017-11-15 16:01:08 -0800

    setup: init repos
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- setup2.sh
+++ setup2.sh
@@ -48,8 +48,10 @@ function packages {
 
 function clonerepo {
   local ghrepo=$1
+  shift
+  local cmds=$*
 
-  if [[ ! $ghrepo =~ \/([a-zA-Z0-1\-]+)\.git ]]; then
+  if [[ ! $ghrepo =~ \/([a-zA-Z0-1\.\-]+)\.git ]]; then
     echo "invalid github repo: $ghrepo"
     exit 1
   fi
@@ -57,32 +59,40 @@ function clonerepo {
   local dir=$HOME/src/${BASH_REMATCH[1]}
   if [ ! -d "$dir" ]; then
     tmpdir=$(mktemp -d)
-    git clone --recursive git@github.com:$1 $tmpdir
+    git clone --recursive git@github.com:${ghrepo} $tmpdir
     mv $tmpdir $dir
+
+    if [ ! -z "${cmds}" ]; then
+      pushd ${dir}
+      exec ${cmds} || true
+      popd
+    fi
   fi
 }
 
 function gitrepos {
   mkdir -p $HOME/src
 
+  clonerepo cruzdb/cruzdb.git
   clonerepo noahdesu/zlog.git
   clonerepo noahdesu/thesis.git
-  clonerepo noahdesu/newsite.git
+  clonerepo noahdesu/nwat.io.git
   clonerepo ceph/ceph.git
-  clonerepo noahdesu/decl-storage-hs17
-  clonerepo michaelsevilla/malacology-popper
+  clonerepo cfpdb/cfpdb.git npm install
+  clonerepo cfpdb/cfp.pub.git npm install
 
-  pushd $HOME/src/zlog
-  ./install-deps.sh
-  popd
+  #pushd $HOME/src/zlog
+  #./install-deps.sh
+  #popd
 
-  pushd $HOME/src/ceph
-  ./install-deps.sh
-  popd
+  #pushd $HOME/src/ceph
+  #./install-deps.sh
+  #popd
 
-  pushd $HOME/src/thesis
-  ./build.sh
-  popd
+  # TODO: don't run these if they have been run
+  #pushd $HOME/src/thesis
+  #./build.sh
+  #popd
 }
 
 function link() {
